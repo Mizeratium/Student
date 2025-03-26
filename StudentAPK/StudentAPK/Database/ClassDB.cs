1821984d920eb98141752504067344d9ac2b87bf
Mizeratium <yamina1105@gmail.com>
2025-03-26 12:20:03 +0300

    Добавление бд

---
+++ StudentAPK/StudentAPK/Database/ClassDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAPK.Database
+{
+    public class ClassDB
+    {
+        public static StudentDBEntities connection = new StudentDBEntities();
+    }
+}
